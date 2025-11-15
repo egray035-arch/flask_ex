6d8e6bb28087dd61af0d4a88e7df78c571251d3e
Gabe Dorval <gabedorval589@gmail.com>
2025-11-15 07:48:02 -0800

    INitial commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask render_template
+
+app = Flask(__main__)
+
+@app.route("/")
+def hello():
+    return "this is amazing"
+
+@app.route('main')
+def home():
+    return(render_template)('index.html')
+
+app.run(host='')
